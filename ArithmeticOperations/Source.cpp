7323b4470ca68c0ce593efb15848ea8ec1edd7c8
Vitaliy <vitaliypolyanskiy@gmail.com>
2024-11-12 14:10:33 +0200

    Add project files.

---
+++ ArithmeticOperations/Source.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number;
+	cout << "Enter a three-digit number: ";
+	cin >> number; 
+	int digit1, digit2, digit3;
+	digit1 = number / 100; // старший разряд числа
+	digit2 = number / 10 % 10; // средний разряд числа
+	digit3 = number % 10; // младший разряд числа
+	cout << "Sum of digits: " << digit1 + digit2 + digit3 << endl;
+	
+	return 0;
+}
